2f8873a5b8aa660651862e74e61909f78ae1a957
Yash Singh <ys181544@gmail.com>
2025-11-03 19:17:35 +0530

    new

---
+++ Shield.java
@@ -0,0 +1,16 @@
+class Avengers {
+
+    void mission() {
+        System.out.println("mission started....");
+    }
+}
+
+public class Shield {
+
+    public static void main(String[] args) {
+
+        Avengers a = new Avengers();
+        a.mission();
+    }
+
+}
