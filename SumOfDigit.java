9e892d462ae38105bcddec42a30e42d1ed80fbe6
Yash Singh <ys181544@gmail.com>
2025-11-19 01:54:01 +0530

    new

--- SumOfDigit.java
+++ SumOfDigit.java
@@ -1,11 +1,21 @@
 import java.util.*;
 class SumOfDigit{
-    public static void main(String[] args) {
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int n = sc.nextInt(), sum = 0;
+    //     while (n > 0){
+    //         sum += n % 10;
+    //         n /= 10;
+    //     }
+    //     System.out.println(sum);
+    // }
+    public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), sum = 0;
-        while (n > 0){
+        while (n>0){
             sum += n % 10;
-            n /= 10;
+            n /=10;
+
         }
         System.out.println(sum);
     }
